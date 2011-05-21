4b4e75cdc5965c08bbb5f2ca10bdb73412917895
Richard Fobes <prog@SolutionsCreative.com>
2011-05-20 22:14:51 -0700

    add main calling script (sample standard usage)

---
+++ dashrep_do_translation.pl
@@ -0,0 +1,12 @@
+#-----------------------------------------------
+#
+#      Do Dashrep translation
+#      ----------------------
+#
+#-----------------------------------------------
+
+
+use dashrep_translate;
+
+&dashrep_translate::dashrep_linewise_translate( );
+
