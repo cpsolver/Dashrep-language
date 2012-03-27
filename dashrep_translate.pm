1c6fad08ac19fb7a8f1fddb774168f4015fd6469
Richard Fobes <prog@SolutionsCreative.com>
2012-03-27 12:44:57 -0700

    ensure multi-line text strings are split

--- dashrep_translate.pm
+++ dashrep_translate.pm
@@ -3677,7 +3677,7 @@ sub dashrep_internal_split_delimited_items
 #-----------------------------------------------
 #  Convert all delimiters to single commas.
 
-    if ( $text_string =~ /[\n\r]/ )
+    if ( $text_string =~ /[\n\r]/s )
     {
         $text_string =~ s/[\n\r][\n\r]+/,/gs ;
         $text_string =~ s/[\n\r][\n\r]+/,/gs ;
